78e50a5908ba2e2b7cabc74984e2d4e0073a2e62
ANUJ SHARMA <49609133+anuj-it@users.noreply.github.com>
2020-02-26 00:17:19 +0530

    Delete UWCOI 2020 Base Plans UWCOI20D.py

--- UWCOI 2020 Base Plans UWCOI20D.py
+++
@@ -1,7 +0,0 @@
-# cook your dish here
-for _ in range(int(input())) :
-    N = int(input())
-    field = []
-    for i in range(N) :
-        field.append(list(map(int, input())))
-    print(N*(N+1)//2)
