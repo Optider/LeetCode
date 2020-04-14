98ce0fadee83c87ad7c1eeeb02e90335733f8e85
Anuj Sharma <49609133+Optider@users.noreply.github.com>
2020-04-14 23:14:24 +0530

    Day 14 of 30 days challenge

---
+++ 30-day-leetcoding-challenge -> Perform String Shifts.py
@@ -0,0 +1,18 @@
+class Solution:
+    def stringShift(self, s: str, shift: List[List[int]]) -> str:
+        count = 0
+        for i in shift :
+            if i[0] :
+                count += i[1]
+            else :
+                count -= i[1]
+        length = len
+        if count > 0 :
+            count = count % length(s)
+            return s[length(s)-count:]+s[:length(s)-count]
+        elif count < 0 :
+            count = ( count % length(s) ) - length(s)
+            count *= -1 # since count is -ve
+            return s[count:] + s[:count]
+        else :
+            return s
