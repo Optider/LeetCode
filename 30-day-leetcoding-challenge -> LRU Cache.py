9e051d4e0fe68eb72171ae5d7d27bb66d7507a31
Anuj Sharma <49609133+Optider@users.noreply.github.com>
2020-04-24 21:32:26 +0530

    Day 24 of 30 days challenge

---
+++ 30-day-leetcoding-challenge -> LRU Cache.py
@@ -0,0 +1,66 @@
+from collections import OrderedDict
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.size = capacity
+        self.cache = OrderedDict()
+    
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        else:
+            self.cache.move_to_end(key)  # Gotta keep this pair fresh, move to end of OrderedDict
+            return self.cache[key]
+
+    def put(self, key: int, value: int) -> None:
+        if key not in self.cache:
+            if len(self.cache) >= self.size:
+                self.cache.popitem(last=False) # last=True, LIFO; last=False, FIFO. We want to remove in FIFO fashion. 
+        else:
+            self.cache.move_to_end(key) # Gotta keep this pair fresh, move to end of OrderedDict    
+        self.cache[key] = value
+        
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
+
+# Alter (Slower)
+'''
+from collections import deque
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.size = capacity
+        self.deque = deque()
+    
+    def _find(self, key: int) -> int:
+		""" Linearly scans the deque for a specified key. Returns -1 if it does not exist, returns the index of the deque if it exists"""
+        for i in range(len(self.deque)):
+            n = self.deque[i]
+            if n[0] == key:
+                return i 
+        return -1
+    
+    def get(self, key: int) -> int:
+        idx = self._find(key)
+        if idx == -1:
+            return -1
+        else:
+            k, v = self.deque[idx]
+            del self.deque[idx] # We have to put this pair at the end of the queue so, we have to delete it first
+            self.deque.append((k, v)) # And now, we put it at the end of the queue. So, the most viewed ones will be always at the end and be saved from popping when new capacity got hit.
+            return v
+                
+
+    def put(self, key: int, value: int) -> None:
+        idx = self._find(key)
+        if idx == -1:
+            if len(self.deque) >= self.size:
+                self.deque.popleft()
+            self.deque.append((key,value))
+        else:
+            del self.deque[idx]
+            self.deque.append((key, value))'''
